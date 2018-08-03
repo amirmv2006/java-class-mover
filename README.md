16b18cf50483e6796fe35bdda4719a021a87a626
Amir <amirmv2006@gmail.com>
2018-08-03 21:17:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Utility project to move java classes.
+I'm using this to refactor/move my generated classes into sub-modules.
---
+++ src/main/java/ir/amv/os/utils/javaclassmover/JavaClassMoverApplication.java
@@ -0,0 +1,12 @@
+package ir.amv.os.utils.javaclassmover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaClassMoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaClassMoverApplication.class, args);
+	}
+}
